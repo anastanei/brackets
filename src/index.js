c0133e7218035380cbba316478c5c67dc88a5848
Anastasiia Nikonova <anastaney@gmail.com>
2024-10-18 10:58:51 +0200

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const brackets = {};
+
+  bracketsConfig.forEach((item) => {
+    const key = item[0];
+    const value = item[1];
+    brackets[key] = value;
+  });
+
+  const isValide = str.split('').every((char) => {
+    if (brackets[char]) {
+      if (brackets[char] === char) {
+        if (stack[stack.length - 1] === char) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
+      } else {
+        stack.push(char);
+      }
+    } else if (Object.values(brackets).includes(char)) {
+      const openBracket = stack.pop();
+      if (brackets[openBracket] !== char) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+  return isValide && stack.length === 0;
+};
